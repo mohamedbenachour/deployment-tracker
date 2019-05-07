3c5ba2f68cf8c47caafbd7061e6d3de6a4f2d071
Pramod Dematagoda <pmdematagoda@mykolab.ch>
2019-05-07 14:37:26 +1000

    Sort deployments based on modified timestamp.
    Also show the modified timestamp in the deployments list.

--- Frontend/src/deployment/selectors.js
+++ Frontend/src/deployment/selectors.js
@@ -1,14 +1,22 @@
 import { createSelector } from 'reselect'
 
 import { getDeployments } from '../environment/selectors'
+import { deploymentIsDestroyed } from './deployment-status';
 
 export const getBranchNameFilter = ({ deployment: { filters: { branchName } }}) => branchName;
 
 export const getShowDestroyed = ({ deployment: {filters: { showDestroyed }}}) => showDestroyed;
 
+export const sortDeployments = (deployments) => deployments.sort((dOne, dTwo) => {
+    const dOneDate = Date.parse(dOne.modifiedBy.timestamp);
+    const dTwoDate = Date.parse(dTwo.modifiedBy.timestamp);
+
+    return dOneDate < dTwoDate;
+});
+
 export const getVisibleDeployments = createSelector(
     [getDeployments, getBranchNameFilter, getShowDestroyed],
     (deployments, branchNameFilter, showDestroyed) =>
-        deployments
+        sortDeployments(deployments
         .filter(({ branchName }) => branchName.includes(branchNameFilter))
-        .filter(({ status }) => status === 'RUNNING' || (status === 'DESTROYED' && showDestroyed)));
+        .filter(({ status }) => status === 'RUNNING' || (status === 'DESTROYED' && showDestroyed))));
