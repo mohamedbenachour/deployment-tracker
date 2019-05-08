7a37967247035cc1cf219bca2c0807c223b47f89
Pramod Dematagoda <pmdematagoda@mykolab.ch>
2019-05-09 02:06:42 +1000

    Move to razor pages as these are simpler and keep the code together in one place.

--- Startup.cs
+++ Startup.cs
@@ -101,12 +101,7 @@ namespace deployment_tracker
             app.UseCookiePolicy();
             app.UseAuthentication();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvc();
         }
     }
 }
