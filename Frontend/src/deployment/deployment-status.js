e854eaec8c7ee4b7d159378b21941899d0a592ab
Pramod Dematagoda <pmdematagoda@mykolab.ch>
2019-06-10 20:17:30 +1000

    Make the branch name search case-insensitive.

--- Frontend/src/deployment/deployment-status.js
+++ Frontend/src/deployment/deployment-status.js
@@ -2,6 +2,7 @@ export const Running = 'RUNNING';
 export const Destroyed = 'DESTROYED';
 
 export const statusIsRunning = (status) => status === Running;
+export const statusIsDestroyed = (status) => status === Destroyed;
 
 export const deploymentIsRunning = ({ status }) => statusIsRunning(status);
-export const deploymentIsDestroyed = ({ status }) => status === Destroyed;
+export const deploymentIsDestroyed = ({ status }) => statusIsDestroyed(status);
