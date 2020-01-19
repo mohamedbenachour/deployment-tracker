e3e5fe0c03fd0f6cc336eba303e143e4997ebd03
Pramod Dematagoda <pmdematagoda@mykolab.ch>
2020-01-19 21:42:57 +1100

    Setup JS testing.
    Add tests around the app reducer.

---
+++ DeploymentTrackerCore/Frontend/__tests__/app/reducer_spec.js
@@ -0,0 +1,22 @@
+import reducer from '../../src/app/reducer';
+import { sectionChanged } from '../../src/app/actions';
+
+describe('app reducer', () => {
+
+    describe('default state', () => {
+        const defaultState = reducer(undefined, { action: '___' });
+
+        it('has the expected section set', () => {
+            expect(defaultState.section).toBe('deployments');
+        });
+
+        describe('when the app section has changed', () => {
+            const newSection = 'say-what?';
+            const newSectionState = reducer(defaultState, sectionChanged(newSection));
+
+            it('has the new section set', () => {
+                expect(newSectionState.section).toBe(newSection);
+            });
+        });
+    });
+});
