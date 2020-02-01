2e4d6d418eb85a356c7bf9b233c6b3c0a4c30650
Pramod Dematagoda <pmdematagoda@mykolab.ch>
2020-02-01 19:22:49 +1100

    Add in license headers and clean up unnecessary usings.

--- IntegrationTests/Helpers/TestApplicationFactoryExtensions.cs
+++ IntegrationTests/Helpers/TestApplicationFactoryExtensions.cs
@@ -1,4 +1,20 @@
-using System;
+/*
+* This file is part of Deployment Tracker.
+* 
+* Deployment Tracker is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Deployment Tracker is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Deployment Tracker. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -10,7 +26,8 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace IntegrationTests.Helpers {
+namespace IntegrationTests.Helpers
+{
     public static class TestApplicationFactoryExtensions {
 
         public static async Task<HttpClient> GetAuthenticatedClient(this TestApplicationFactory<DeploymentTrackerCore.Startup> factory) {
--- IntegrationTests/TestEnvironment.cs
+++ IntegrationTests/TestEnvironment.cs
@@ -1,19 +1,33 @@
+/*
+* This file is part of Deployment Tracker.
+* 
+* Deployment Tracker is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Deployment Tracker is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Deployment Tracker. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 using IntegrationTests.EnvironmentSetup;
 
-public class TestEnvironment {
-    public const string ExternalToken = "foobar";
+namespace IntegrationTests {
+    public class TestEnvironment {
+        public const string ExternalToken = "foobar";
 
-    public struct User {
-        public const string UserName = "jenkins-user";
-        public const string Password = "test-password";
+        public struct URLs {
+            public const string Environment = "/api/environment";
+            public const string Deployment = "/api/deployment";
+
+            public const string DeploymentExternal = "/api/deployment/external";
+        }
+
+        public static TestApplicationFactory<DeploymentTrackerCore.Startup> ClientFactory = new TestApplicationFactory<DeploymentTrackerCore.Startup>();
     }
-
-    public struct URLs {
-        public const string Environment = "/api/environment";
-        public const string Deployment = "/api/deployment";
-
-        public const string DeploymentExternal = "/api/deployment/external";
-    }
-
-    public static TestApplicationFactory<DeploymentTrackerCore.Startup> ClientFactory = new TestApplicationFactory<DeploymentTrackerCore.Startup>();
 }
