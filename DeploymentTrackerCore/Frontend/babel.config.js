259246bf3dceed32c1797a8393baa28de13f3cfc
Pramod Dematagoda <pmdematagoda@mykolab.ch>
2020-08-10 15:57:13 +1000

    Introduce Typescript to the project.

--- DeploymentTrackerCore/Frontend/babel.config.js
+++ DeploymentTrackerCore/Frontend/babel.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-react'],
+    presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+        '@babel/preset-typescript',
+    ],
     plugins: [
         '@babel/plugin-proposal-object-rest-spread',
-        ["import", { "libraryName": "antd", "libraryDirectory": "es", "style": "css" }] // `style: true` for less
-    ]
+        ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], // `style: true` for less
+    ],
 };
