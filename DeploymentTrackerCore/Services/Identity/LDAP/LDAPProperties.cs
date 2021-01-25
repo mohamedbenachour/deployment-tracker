e1a111003cf648231aa7dc8de1575ca696ae8047
Pramod Dematagoda <pmdematagoda@mykolab.ch>
2021-01-25 16:04:05 +1100

    Use the correct attribute to get the email from.

--- DeploymentTrackerCore/Services/Identity/LDAP/LDAPProperties.cs
+++ DeploymentTrackerCore/Services/Identity/LDAP/LDAPProperties.cs
@@ -2,9 +2,9 @@ using DeploymentTrackerCore.Shared;
 
 namespace DeploymentTrackerCore.Services.Identity.LDAP {
     public class LDAPProperties : Enumeration {
-        public static readonly LDAPProperties UserName = new LDAPProperties(1, "samaccountname");
-        public static readonly LDAPProperties DisplayName = new LDAPProperties(2, "displayname");
-        public static readonly LDAPProperties Email = new LDAPProperties(3, "email");
+        public static readonly LDAPProperties UserName = new(1, "samaccountname");
+        public static readonly LDAPProperties DisplayName = new(2, "displayname");
+        public static readonly LDAPProperties Email = new(3, "mail");
 
         private LDAPProperties(int id, string name) : base(id, name) {
 
